59a6a00e8cdba7a7b66461ab908abed009855777
鹿鸣 <85821538+fengyec2@users.noreply.github.com>
2025-06-08 13:08:10 +0800

    Refactor page_class_grade UI to enhance layout structure, improve styling, and add functionality for query and result display

--- ui_page/ui_page_class_grade.py
+++ ui_page/ui_page_class_grade.py
@@ -15,80 +15,127 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QComboBox, QHBoxLayout,
-    QHeaderView, QLabel, QLineEdit, QListWidget,
-    QListWidgetItem, QPushButton, QSizePolicy, QTabWidget,
-    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QComboBox, QFrame,
+    QHBoxLayout, QHeaderView, QLabel, QLineEdit,
+    QListWidget, QListWidgetItem, QPushButton, QSizePolicy,
+    QTabWidget, QTableWidget, QTableWidgetItem, QVBoxLayout,
+    QWidget)
 
 class Ui_page_class_grade(object):
     def setupUi(self, page_class_grade):
         if not page_class_grade.objectName():
             page_class_grade.setObjectName(u"page_class_grade")
         page_class_grade.resize(800, 600)
-        self.verticalLayout = QVBoxLayout(page_class_grade)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.btnRefresh = QPushButton(page_class_grade)
-        self.btnRefresh.setObjectName(u"btnRefresh")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btnRefresh.sizePolicy().hasHeightForWidth())
-        self.btnRefresh.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(page_class_grade.sizePolicy().hasHeightForWidth())
+        page_class_grade.setSizePolicy(sizePolicy)
+        self.verticalLayout = QVBoxLayout(page_class_grade)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frameQueryCard = QFrame(page_class_grade)
+        self.frameQueryCard.setObjectName(u"frameQueryCard")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.frameQueryCard.sizePolicy().hasHeightForWidth())
+        self.frameQueryCard.setSizePolicy(sizePolicy1)
+        self.frameQueryCard.setMaximumSize(QSize(16777215, 100))
+        self.frameQueryCard.setStyleSheet(u"QFrame {\n"
+"    background-color: white;\n"
+"    border-radius: 10px;\n"
+"    border: 1px solid #dcdcdc;\n"
+"}")
+        self.frameQueryCard.setFrameShape(QFrame.Shape.NoFrame)
+        self.horizontalLayout = QHBoxLayout(self.frameQueryCard)
+        self.horizontalLayout.setSpacing(10)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.btnRefresh = QPushButton(self.frameQueryCard)
+        self.btnRefresh.setObjectName(u"btnRefresh")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.btnRefresh.sizePolicy().hasHeightForWidth())
+        self.btnRefresh.setSizePolicy(sizePolicy2)
 
         self.horizontalLayout.addWidget(self.btnRefresh)
 
-        self.label = QLabel(page_class_grade)
+        self.label = QLabel(self.frameQueryCard)
         self.label.setObjectName(u"label")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy3)
 
         self.horizontalLayout.addWidget(self.label)
 
-        self.comboExam = QComboBox(page_class_grade)
+        self.comboExam = QComboBox(self.frameQueryCard)
         self.comboExam.setObjectName(u"comboExam")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.comboExam.sizePolicy().hasHeightForWidth())
-        self.comboExam.setSizePolicy(sizePolicy1)
-        self.comboExam.setMinimumSize(QSize(200, 0))
+        self.comboExam.setMinimumSize(QSize(150, 0))
 
         self.horizontalLayout.addWidget(self.comboExam)
 
-        self.label_2 = QLabel(page_class_grade)
+        self.label_2 = QLabel(self.frameQueryCard)
         self.label_2.setObjectName(u"label_2")
 
         self.horizontalLayout.addWidget(self.label_2)
 
-        self.listClasses = QListWidget(page_class_grade)
+        self.listClasses = QListWidget(self.frameQueryCard)
         self.listClasses.setObjectName(u"listClasses")
+        self.listClasses.setMinimumSize(QSize(100, 0))
+        self.listClasses.setStyleSheet(u"\n"
+"           QListView {\n"
+"             border: 1px solid #dcdfe6;\n"
+"             border-radius: 4px;\n"
+"             padding: 4px;\n"
+"             background-color: #ffffff;\n"
+"           }\n"
+"           QListView::item {\n"
+"             padding: 8px 12px;\n"
+"             border-bottom: 1px solid #ebeef5;\n"
+"           }\n"
+"           QListView::item:hover {\n"
+"             background-color: #f5f7fa;\n"
+"           }\n"
+"           QListView::item:selected {\n"
+"             background-color: #ecf5ff;\n"
+"             color: #409eff;\n"
+"             border-left: 3px solid #409eff;\n"
+"           }\n"
+"         ")
         self.listClasses.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
 
         self.horizontalLayout.addWidget(self.listClasses)
 
-        self.btnQuery = QPushButton(page_class_grade)
+        self.btnQuery = QPushButton(self.frameQueryCard)
         self.btnQuery.setObjectName(u"btnQuery")
-        sizePolicy.setHeightForWidth(self.btnQuery.sizePolicy().hasHeightForWidth())
-        self.btnQuery.setSizePolicy(sizePolicy)
+        sizePolicy2.setHeightForWidth(self.btnQuery.sizePolicy().hasHeightForWidth())
+        self.btnQuery.setSizePolicy(sizePolicy2)
 
         self.horizontalLayout.addWidget(self.btnQuery)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout.addWidget(self.frameQueryCard)
 
-        self.tabWidget = QTabWidget(page_class_grade)
-        self.tabWidget.setObjectName(u"tabWidget")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
-        self.tabWidget.setSizePolicy(sizePolicy2)
-        self.tabWidget.setMinimumSize(QSize(0, 450))
-        self.tabTable = QWidget()
-        self.tabTable.setObjectName(u"tabTable")
-        self.verticalLayout_2 = QVBoxLayout(self.tabTable)
+        self.frameResultCard = QFrame(page_class_grade)
+        self.frameResultCard.setObjectName(u"frameResultCard")
+        self.frameResultCard.setStyleSheet(u"QFrame {\n"
+"    background-color: white;\n"
+"    border-radius: 10px;\n"
+"    border: 1px solid #dcdcdc;\n"
+"}")
+        self.frameResultCard.setFrameShape(QFrame.Shape.NoFrame)
+        self.verticalLayout_2 = QVBoxLayout(self.frameResultCard)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.tableScores = QTableWidget(self.tabTable)
+        self.tabWidget = QTabWidget(self.frameResultCard)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setMinimumSize(QSize(0, 400))
+        self.tabTable_2 = QWidget()
+        self.tabTable_2.setObjectName(u"tabTable_2")
+        self.verticalLayout_table_2 = QVBoxLayout(self.tabTable_2)
+        self.verticalLayout_table_2.setObjectName(u"verticalLayout_table_2")
+        self.tableScores = QTableWidget(self.tabTable_2)
         if (self.tableScores.columnCount() < 5):
             self.tableScores.setColumnCount(5)
         __qtablewidgetitem = QTableWidgetItem()
@@ -105,22 +152,25 @@ class Ui_page_class_grade(object):
         self.tableScores.setRowCount(0)
         self.tableScores.setColumnCount(5)
 
-        self.verticalLayout_2.addWidget(self.tableScores)
+        self.verticalLayout_table_2.addWidget(self.tableScores)
 
-        self.tabWidget.addTab(self.tabTable, "")
-        self.tabChart = QWidget()
-        self.tabChart.setObjectName(u"tabChart")
-        self.verticalLayout_3 = QVBoxLayout(self.tabChart)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.widgetChart = QWidget(self.tabChart)
+        self.tabWidget.addTab(self.tabTable_2, "")
+        self.tabChart_2 = QWidget()
+        self.tabChart_2.setObjectName(u"tabChart_2")
+        self.verticalLayout_4 = QVBoxLayout(self.tabChart_2)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.widgetChart = QWidget(self.tabChart_2)
         self.widgetChart.setObjectName(u"widgetChart")
         self.widgetChart.setStyleSheet(u"background-color: #f0f0f0;")
 
-        self.verticalLayout_3.addWidget(self.widgetChart)
+        self.verticalLayout_4.addWidget(self.widgetChart)
 
-        self.tabWidget.addTab(self.tabChart, "")
+        self.tabWidget.addTab(self.tabChart_2, "")
 
-        self.verticalLayout.addWidget(self.tabWidget)
+        self.verticalLayout_2.addWidget(self.tabWidget)
+
+
+        self.verticalLayout.addWidget(self.frameResultCard)
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
@@ -139,6 +189,9 @@ class Ui_page_class_grade(object):
 
 
         self.retranslateUi(page_class_grade)
+
+        self.tabWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(page_class_grade)
     # setupUi
@@ -159,8 +212,8 @@ class Ui_page_class_grade(object):
         ___qtablewidgetitem3.setText(QCoreApplication.translate("page_class_grade", u"\u82f1\u8bed", None));
         ___qtablewidgetitem4 = self.tableScores.horizontalHeaderItem(4)
         ___qtablewidgetitem4.setText(QCoreApplication.translate("page_class_grade", u"\u5e73\u5747\u5206", None));
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabTable), QCoreApplication.translate("page_class_grade", u"\u8868\u683c\u89c6\u56fe", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabChart), QCoreApplication.translate("page_class_grade", u"\u56fe\u8868\u89c6\u56fe", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabTable_2), QCoreApplication.translate("page_class_grade", u"\u8868\u683c\u89c6\u56fe", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabChart_2), QCoreApplication.translate("page_class_grade", u"\u56fe\u8868\u89c6\u56fe", None))
         self.labelStats.setText(QCoreApplication.translate("page_class_grade", u"\u5e74\u7ea7\u5e73\u5747\u5206\uff1a--", None))
         self.editNotes.setPlaceholderText(QCoreApplication.translate("page_class_grade", u"\u8f93\u5165\u5907\u6ce8...", None))
     # retranslateUi
