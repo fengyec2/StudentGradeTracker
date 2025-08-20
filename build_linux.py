8fa0a3b3c0d82afe533cfb6a31fef6588d5a7586
鹿鸣 <85821538+fengyec2@users.noreply.github.com>
2025-08-20 17:23:46 +0800

    fix: update Linux build script to use correct icon file

--- build_linux.py
+++ build_linux.py
@@ -4,7 +4,7 @@ from common.config import VERSION, AUTHOR
 app_name = 'StudentGradeTracker'
 build_command = "nuitka --standalone --enable-plugin=pyside6 "
 build_command += "--assume-yes-for-downloads "
-build_command += "--linux-icon=resource/images/logo.png --output-dir=out "
+build_command += "--linux-icon=resource/images/logo.ico --output-dir=out "
 build_command += f"--linux-onefile-icon=resource/images/logo.ico "
 build_command += f"--product-name={app_name} "
 build_command += f"--product-version={VERSION} "
